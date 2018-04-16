ba4dc2c7f85b10397262109361588498128d485b
@Suresh <swissbhs@gmail.com>
2018-04-16 09:54:46 -0600

    Minor uptade

--- js.js
+++ js.js
@@ -2,13 +2,13 @@ var age = prompt("how old are you?")
 
 if (age < 18)
 {
-	console.log("you can not enter");
+	console.log("you can not enter the club");
 }
 else if (age>18 && age<21)
 {
-	console.log("you can enter but can't drink");
+	console.log("you can enter the club but can't drink any alchohal");
 }
 else
 {
-	console.log("enjoy your drink");
+	console.log("enjoy your alchoholic drinks");
 }
